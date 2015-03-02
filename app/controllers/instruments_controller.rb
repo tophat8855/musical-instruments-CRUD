924133b638553ddc8520d55d2e8214108da92f78
tophat8855 <hrfarley@gmail.com>
2015-03-02 15:45:23 -0800

    can delete an instrument

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -14,6 +14,21 @@ class InstrumentsController < ApplicationController
     end
   end
 
+  def show
+    @instrument = Instrument.find(params[:id])
+  end
+
+  def delete
+    @instrument = Instrument.find(params[:id])
+  end
+
+  def destroy
+    @instrument = Instrument.find(params[:id])
+    if @instrument.destroy
+      redirect_to instruments_path
+    end
+  end
+
   private
   def instrument_params
     params.require(:instrument).permit(:name, :group, :price)
