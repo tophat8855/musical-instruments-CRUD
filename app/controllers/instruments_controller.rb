d0d63fd59fd9a90fc27c6f169010e592d10877d4
tophat8855 <hrfarley@gmail.com>
2015-03-02 15:32:15 -0800

    Create finished

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -6,4 +6,16 @@ class InstrumentsController < ApplicationController
   def new
     @instrument = Instrument.new
   end
+
+  def create
+    @instrument = Instrument.new(instrument_params)
+    if @instrument.save
+      redirect_to instruments_path
+    end
+  end
+
+  private
+  def instrument_params
+    params.require(:instrument).permit(:name, :group, :price)
+  end
 end
